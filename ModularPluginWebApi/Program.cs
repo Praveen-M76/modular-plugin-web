fceffd343478d71c8593ab8662b7cf7fd385e608
Praveen-M76 <praveenmathu20@gmail.com>
2026-03-05 10:38:51 +0530

    Update Program.cs

--- ModularPluginWebApi/Program.cs
+++ ModularPluginWebApi/Program.cs
@@ -1,6 +1,22 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+var pluginPath = Path.Combine(Directory.GetCurrentDirectory(), "Plugins");
+
+if (Directory.Exists(pluginPath))
+{
+    foreach (var file in Directory.GetFiles(pluginPath, "*.dll"))
+    {
+        var assembly = Assembly.LoadFrom(file);
+        builder.Services.AddControllers()
+            .PartManager.ApplicationParts.Add(new AssemblyPart(assembly));
+    }
+}
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
